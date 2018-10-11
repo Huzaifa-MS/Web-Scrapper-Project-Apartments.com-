145be1904ae0d738774871bccb594645d3d99996
muhammadHuzaifaSalman <43180782+muhammadHuzaifaSalman@users.noreply.github.com>
2018-10-11 18:14:32 -0500

    Update Program.cs
    hmmm it works?

--- Program.cs
+++ Program.cs
@@ -5,71 +5,198 @@ using System.Text;
 using System.Net;
 using HtmlAgilityPack;
 using System.Net.Http;
+using System.Collections;
+using System.IO;
 
 namespace WebScrapper
 {
     class Program
     {
-       static void Main(string[] args)
-       {
+        static void Main(string[] args)
+        {
             GetHtmlAsync();
             Console.ReadLine();
-       }
+        }
 
         static async void GetHtmlAsync()
         {
-            /// Need to implement null checks and exceptions.
-            
-            var url = "https://www.apartments.com/frisco-tx-75035/";
-            var httpClient = new HttpClient();
+            StreamWriter outFile = new StreamWriter("C:/Users/scale/OneDrive/Desktop/VS Studios Code/WScrapper/WScrapper/ScrapperData.txt");
 
-            var html = await httpClient.GetStringAsync(url);
-            
-            var htmlDocument = new HtmlDocument(); // Document object is made
-            htmlDocument.LoadHtml(html); // html is loaded into the dom.
-                                         // parses 
-
-            // This code goes through the whole page and gets any data with the :
-            // Html tag "article", if seen in inspect mode on the web page the word is in purple.
-            // The var type "class", in the color yellow
-            // and contains the name "placard" in its variable name.
-            var apartmentListings = htmlDocument.DocumentNode.Descendants("article") // html tag
-                               .Where(node => node.GetAttributeValue("class", "") // var type
-                               .Contains("placard")).ToList(); // var Name
-            // Have to consider other data that may adhere to this such as ads.
-
-            foreach(var apartment in apartmentListings)
+            var url = "https://www.apartments.com/frisco-tx-75034/"; // Starting url
+            do
             {
-                // Gets the name of the apartment. Still need to write exception for ads
-                var apartmentName = apartment.Descendants("a") 
-                               .Where(node => node.GetAttributeValue("class", "") 
-                               .Equals("placardTitle js-placardTitle  "));
+                outFile.WriteLine("\n");
 
-                // Gets the location listed for the apartment
-                var apartmentLocation = apartment.Descendants("div")
-                                        .Where(node => node.GetAttributeValue("class", "")
-                                        .Equals("location"));
+                var httpClient = new HttpClient();
 
-                // Price Listing
-                var apartmentRent = apartment.Descendants("span")
+                var html = await httpClient.GetStringAsync(url);
+
+                var htmlDocument = new HtmlDocument(); // Document object is made
+                htmlDocument.LoadHtml(html); // html is loaded into the dom.
+                                             // parses 
+
+                // Get the unordered list of apartments
+                var apartmentListing = htmlDocument.DocumentNode.Descendants("div") // html tag
+                                   .Where(node => node.GetAttributeValue("id", "") // var type
+                                   .Equals("placardContainer")).ToList(); // var Name
+
+                // Gets each apartment in the list instead of its url for some reason
+                var apartments = apartmentListing[0].Descendants("article")
+                                    .Where(node => node.GetAttributeValue("data-url", "")
+                                    .Contains("www.apartments.com")).ToList();
+
+
+                // Gets the last element in the list of apartments, A.K.A the arrow bar
+                var nextPage = apartmentListing[0].Descendants("a")
                                     .Where(node => node.GetAttributeValue("class", "")
-                                    .Equals("altRentDisplay"));
+                                    .Equals("next ")).ToList();
 
-                // Phone #
-                var apartmentContactInfo = apartment.Descendants("div")
-                                    .Where(node => node.GetAttributeValue("class", "")
-                                    .Equals("phone")).ToList();
-                var apartmentPhoneNum = apartmentContactInfo[0].InnerText.Trim(); // Need to see why info is so spaced out.
+                // Gets link to the next page.
+                string nextPageUrl = "";
+                if (nextPage.Count() == 0)
+                {
+                    nextPage = null;
+                }
+                else
+                {
+                    nextPageUrl = nextPage[0].Attributes["href"].Value;
+                }
 
-                if (apartmentName != null)
-                // Gives error when trying to write from an ad.
-                Console.WriteLine(apartmentName.ElementAt(0).InnerHtml); // InnerHtml allows us to out the data as a string.
-                Console.WriteLine(apartmentLocation.ElementAt(0).InnerHtml);
-                Console.WriteLine(apartmentRent.ElementAt(0).InnerHtml);
-                Console.WriteLine(apartmentPhoneNum);
-            }
+                ArrayList urls = new ArrayList(); 
+                // Need to make because once we go through the apartments list and scrape data
+                // the apartments variable get scrambled for some reason.
 
-            Console.WriteLine();
+                for(int i = 0; i < apartments.Count(); i++)
+                {
+                    var apartmentUrl = apartments[i].Descendants("a")
+                                        .Where(node => node.GetAttributeValue("href", "")
+                                        .Contains("www.apartments.com")).ToList();
+                    urls.Add(apartmentUrl[0].GetAttributeValue("href", ""));
+                }
+                
+                // Allows access into each apartment.
+                for(int i = 0; i < apartments.Count(); i ++)
+                        /// change back to 0
+                {
+                    //ApartmentUrl
+
+                    // gets the link to apartment page.
+                    var aP = urls[i].ToString();
+
+                    // Goes inside the apartments webpage.
+                    if (urls[i] != null)
+                    {
+                        var urlNew = aP;
+
+                        var htmlNew = await httpClient.GetStringAsync(urlNew);
+
+                        //var htmlDocumentNew = new HtmlDocument(); // Document object is made
+
+                        htmlDocument.LoadHtml(htmlNew);
+
+                        var apartmentTable = htmlDocument.DocumentNode.Descendants("div")
+                                             .Where(node => node.GetAttributeValue("class", "")
+                                             .Equals("tabContent active")).ToList();
+
+                        if (apartmentTable.Count() == 0) // For basic apartments
+                        {
+                            apartmentTable = htmlDocument.DocumentNode.Descendants("table")
+                                             .Where(node => node.GetAttributeValue("class", "")
+                                             .Equals("availabilityTable basic oneRental")).ToList();
+                        }
+                        if (apartmentTable.Count() == 0)
+                        {
+                            apartmentTable = htmlDocument.DocumentNode.Descendants("table")
+                                             .Where(node => node.GetAttributeValue("class", "")
+                                             .Equals("availabilityTable tiertwo")).ToList();
+                        }
+
+                        var apartmentModelList = apartmentTable[0].Descendants("tr")
+                                             .Where(node => node.GetAttributeValue("class", "")
+                                             .Contains("rentalGridRow")).ToList();
+
+                        outFile.WriteLine("Models of the Apartment : \n");
+
+                        foreach (var model in apartmentModelList)
+                        {
+                            var bedrooms = (model.Descendants("td")
+                                .Where(node => node.GetAttributeValue("class", "")
+                                .Contains("beds")).ToList())[0].Descendants("span")
+                                .Where(node => node.GetAttributeValue("class", "")
+                                .Equals("shortText")).ToList();
+
+                            var baths = (model.Descendants("td")
+                                .Where(node => node.GetAttributeValue("class", "")
+                                .Contains("baths")).ToList())[0].Descendants("span")
+                                .Where(node => node.GetAttributeValue("class", "")
+                                .Equals("shortText")).ToList();
+
+                            var rent = model.Descendants("td")
+                                .Where(node => node.GetAttributeValue("class", "")
+                                .Contains("rent")).ToList();
+
+                            var deposit = model.Descendants("td")
+                                .Where(node => node.GetAttributeValue("class", "")
+                                .Contains("deposit")).ToList();
+
+                            var modelName = model.Descendants("td")
+                               .Where(node => node.GetAttributeValue("class", "")
+                               .Contains("name")).ToList(); //Gives model name as 1A1 for some reason. Webscrapping has been detected by website?
+                                // Actually its giving back the data-model as the name for some reason?
+
+                            var area = model.Descendants("td")
+                               .Where(node => node.GetAttributeValue("class", "")
+                               .Contains("sqft")).ToList();
+
+                            var availability = model.Descendants("td")
+                               .Where(node => node.GetAttributeValue("class", "")
+                               .Contains("availabile")).ToList();
+
+                            if (modelName.Count() != 0 && modelName[0] != null) // going out of bounds?
+                                outFile.WriteLine("ModelName: " + modelName[0].InnerText.Trim());
+                            if (bedrooms.Count() != 0 && bedrooms[0] != null)
+                                outFile.WriteLine("Bedroom: " + bedrooms[0].InnerText.Trim());
+                            if (baths.Count() != 0 && baths[0] != null)
+                                outFile.WriteLine("Bathroom: " + baths[0].InnerText.Trim());
+                            if (rent.Count() != 0 && rent[0] != null)
+                                outFile.WriteLine("Rent: " + rent[0].InnerText.Trim());
+                            if (deposit.Count() != 0 && deposit[0] != null)
+                                outFile.WriteLine("Deposit: " + deposit[0].InnerText.Trim());
+                            if (area.Count() != 0 && area[0] != null)
+                                outFile.WriteLine("Area: " + area[0].InnerText.Trim());
+
+                            outFile.WriteLine();
+
+                        }
+                        outFile.WriteLine("Page Finish");
+
+                        var apartmentDescription = htmlDocument.DocumentNode.Descendants("section")
+                                             .Where(node => node.GetAttributeValue("class", "")
+                                             .Contains("descriptionSection")).ToList();
+
+                        outFile.WriteLine("\nDescription: " + apartmentDescription[0].InnerText.Trim());
+                        outFile.WriteLine();
+
+                    }
+                    outFile.WriteLine();
+                }
+
+                outFile.WriteLine();
+
+                outFile.WriteLine(nextPageUrl);
+
+
+               url = nextPageUrl;
+                if(url == null)
+                {
+                    break;
+                }
+            } while (url != "");
+
+            outFile.Close();
+
+            return;
         }
     }
 }
+
